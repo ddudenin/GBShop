3e7aaf88cc58f6c2da5ab8bcbb9b7b657aedfb50
Dmitry Dudenin <dmitry_dudenin@mail.ru>
2021-08-04 09:20:23 +0300

    Init project and update gitignore

---
+++ OnlineStore/OnlineStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineStore
+//
+//  Created by Дмитрий Дуденин on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
